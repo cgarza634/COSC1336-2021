76eb12b9c2511a4c1b464a41e5fa926321db052f
cgarza634 <81278924+cgarza634@users.noreply.github.com>
2021-03-30 22:39:33 -0500

    Delete Homework 1 directory

--- Homework 1/Businesscard.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Texas Roadhouse" <<std::endl;
-    std::cout <<"6616 W Loop 1604 N, San Antonio, TX" <<std::endl;
-    std::cout <<"+2106887427" <<std::endl;
-    std::cout <<"texasroadhouse.com" <<std::endl;
-}
